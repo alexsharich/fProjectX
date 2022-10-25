e994ed2b615f0c29fbe64d9a0aea92c4c806293b
alexsharich <alexsharich@gmail.com>
2022-10-25 22:15:59 +0200

    change package.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  let [state, setState] = useState([{ name: '', id: undefined }]);
+
+  const getUsers = () => {
+    axios.get('http://localhost:7542/users').then((res) => setState(res.data));
+  };
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  const addUser = () => {
+    axios.post('http://localhost:7542/users').then((res) => {
+      getUsers();
+    });
+  };
+
   return (
-    <div className="App">
-      FPROJECTX
+    <div className='App'>
+      <h1>FPROJECTX</h1>
+      <button onClick={addUser}>Add User</button>
+      <div>{state && state.map((u) => <p key={u.id}>{u.name}</p>)}</div>
     </div>
   );
 }

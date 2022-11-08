58e355bd1ea9adb0426bf97a0bf3e0098a0c209a
alexsharich <alexsharich@gmail.com>
2022-11-08 15:18:22 +0100

    add addImage, create/read/update/deletePost

--- src/pages/FullPost.jsx
+++ src/pages/FullPost.jsx
@@ -5,6 +5,7 @@ import { Post } from "../components/Post";
 import { Index } from "../components/AddComment";
 import { CommentsBlock } from "../components/CommentsBlock";
 import axios from "../axios";
+import ReactMarkdown from "react-markdown";
 
 export const FullPost = () => {
   const [data,setData] = React.useState()
@@ -32,7 +33,7 @@ if(isLoading){
       <Post
         id={data._id}
         title={data.title}
-        imageUrl={data.imageUrl}
+        imageUrl={data.imageUrl ? `http://localhost:4444${data.imageUrl}` : ''}
         user={data.user}
         createdAt={data.createdAt}
         viewsCount={data.viewsCount}
@@ -40,7 +41,7 @@ if(isLoading){
         tags={data.tags}
         isFullPost
       >
-        <p>{data.text}</p>
+       <ReactMarkdown children={data.text} />
       </Post>
       <CommentsBlock
         items={[
--- src/redux/slices/posts.js
+++ src/redux/slices/posts.js
@@ -11,6 +11,11 @@ export const fetchTags = createAsyncThunk('posts/fetchTags',async ()=>{
   return data
  })
 
+ export const fetchRemovePost = createAsyncThunk('posts/fetchRemovePost',async (id)=>{
+  const {data} = await axios.delete(`/posts/${id}`)
+  return data
+ })
+
 const initialState = {
   posts:{
     items:[],
@@ -27,6 +32,7 @@ const postsSlice = createSlice({
   initialState,
   reducers:{},
   extraReducers:{
+    ///
 [fetchPosts.pending]:(state)=>{
   state.posts.items = [];
   state.posts.status = 'loading';
@@ -39,6 +45,7 @@ const postsSlice = createSlice({
   state.posts.items = [];
   state.posts.status = 'error';
 },
+///
 [fetchTags.pending]:(state)=>{
   state.tags.items = [];
   state.tags.status = 'loading';
@@ -51,6 +58,10 @@ const postsSlice = createSlice({
   state.tags.items = [];
   state.tags.status = 'error';
 },
+///
+[fetchRemovePost.pending]:(state,action)=>{
+  state.posts.items = state.posts.items.filter(obj=>obj._id !== action.meta.arg)
+}
   }
 })
 
